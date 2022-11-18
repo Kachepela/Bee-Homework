eb28660b75edbb2eb68fe2d7ebc6cbde5263e85a
Kachepela <kachepela@mail.ru>
2022-11-18 15:57:32 +0400

    Упращеная версия игры Угадайка

--- Guess/Program.cs
+++ Guess/Program.cs
@@ -1,3 +1,21 @@
 ﻿Console.WriteLine("  \t Игра угадайка.\n Угадайте случайное число от 1 до 500");
 int secret = new Random().Next(1, 500);
+Console.WriteLine("Введите число");
+int number;
+int.TryParse(Console.ReadLine(), out number);
+
+if (secret==number )
+{
+    Console.WriteLine(" Поздравляем! Вы угадали. ");
+}
+else if (number<0 || number > 500)
+{
+            Console.WriteLine("Ошибка.Вы ввели не верное значение. ВВедите цифру от 1 до 500.");
+}
+else
+{
+    Console.WriteLine("Иии...барабанная дробь\n  Вы не угадали. ");
+} 
+    
+
 
